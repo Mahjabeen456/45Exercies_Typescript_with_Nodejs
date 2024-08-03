5b3ad6413125785430945a95c9a423cf1f165de9
Mahjabeen <161134560+Mahjabeen456@users.noreply.github.com>
2024-08-03 21:38:53 +0500

    Add files via upload

---
+++ Q4/main.ts
@@ -0,0 +1,6 @@
+/* Q4:Famous Quote: Find a quote from a famous person you admire.
+ Print the quote and the name of its author. Your output should 
+ look something like the following, including the quotation marks:*/
+
+
+console.log('Quiad-e-Azam once said, "There is no power on earth that can undo pakistan." ');
