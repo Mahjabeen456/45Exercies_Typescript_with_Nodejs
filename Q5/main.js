df1c164d3ec7440e8855d83c35da69117cf68e73
Mahjabeen <161134560+Mahjabeen456@users.noreply.github.com>
2024-08-03 21:45:14 +0500

    Add files via upload

---
+++ Q5/main.js
@@ -0,0 +1,7 @@
+/*Famous Quote 2: Repeat Exercise 4, but this time store
+the famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message.
+Print your message.*/
+var famous_person = "Quaid-e-Azam";
+var message = "".concat(famous_person, " once said, \"There is no power on earth that can undo pakistan,\"");
+console.log(message);
---
+++ Q5/main.ts
@@ -0,0 +1,11 @@
+/*Famous Quote 2: Repeat Exercise 4, but this time store
+the famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message.
+Print your message.*/
+
+
+
+
+let famous_person:string = "Quaid-e-Azam";
+let message = `${famous_person} once said, "There is no power on earth that can undo pakistan,"`;
+console.log(message);
