f5b8e370561c12cb8c9553c73a105867747c0897
Mahjabeen <161134560+Mahjabeen456@users.noreply.github.com>
2024-08-07 03:46:48 +0500

    Add files via upload

---
+++ Q32/main.js
@@ -0,0 +1,26 @@
+/*Q32:Checking Usernames: Do the following to create a program that simulates
+ how websites ensure that everyone has a unique username.
+
+• Make a list of five or more usernames called current_users.
+
+• Make another list of five usernames called new_users. Make sure one or two
+of the new usernames are also in the current_users list.
+
+• Loop through the new_users list to see if each new username has already been
+used. If it has, print a message that the person will need to enter a new
+username.If a username has not been used, print a message saying that the
+ username is available.
+
+• Make sure your comparison is case insensitive. If 'John' has been used,
+'JOHN' should not be accepted.*/
+var current_users = ["Ahmeed", "Azeem", "Any", "Noor", "Adil"];
+var new_users = ["Sumya", "Sana", "Azeem", "Yousra", "Any"];
+new_users.forEach(function (new_one_user) {
+    var our_condition = current_users.some(function (current_one_users) { return current_one_users.toLocaleLowerCase() === new_one_user.toLocaleLowerCase(); });
+    if (our_condition) {
+        console.log("Sorry ".concat(new_one_user, " is already taken!"));
+    }
+    else {
+        console.log("This Username ".concat(new_one_user, " is available"));
+    }
+});
