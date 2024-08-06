2521779ab215781720bc66b618f2abc7cc1476c6
Mahjabeen <161134560+Mahjabeen456@users.noreply.github.com>
2024-08-07 02:02:24 +0500

    Add files via upload

---
+++ Q12/main.js
@@ -0,0 +1,6 @@
+/*Q12:Greetings: Start with the array you used in Exercise 11,
+but instead of just printing each person’s name, print a message to them.
+The text of each message should be the same, but each message should be
+personalized with the person’s name.*/
+var friendNames = ["Sana", "Any", "Umaima", "Yusra"];
+friendNames.forEach(function (friendNames) { return console.log("Hello ".concat(friendNames, " How are you")); });
---
+++ Q12/main.ts
@@ -0,0 +1,8 @@
+/*Q12:Greetings: Start with the array you used in Exercise 11,
+but instead of just printing each person’s name, print a message to them.
+The text of each message should be the same, but each message should be
+personalized with the person’s name.*/
+
+let friendNames: string[] = ["Sana", "Any","Umaima","Yusra"];
+friendNames.forEach(friendNames => console.log(`Hello ${friendNames} How are you`));
+
