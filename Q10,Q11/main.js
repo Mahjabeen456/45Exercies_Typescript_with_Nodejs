58d2ee85228527016ccc83761cab33873f2f63a8
Mahjabeen <161134560+Mahjabeen456@users.noreply.github.com>
2024-08-07 01:58:53 +0500

    Add files via upload

---
+++ Q10,Q11/main.js
@@ -0,0 +1,6 @@
+/*Q11:Names: Store the names of a few of your friends in a array called names.
+ Print each person’s name by accessing each element in the list, one at a time.*/
+var friendNames = ['Sana', 'Any', 'Umaima', 'Ayna'];
+for (var i = 0; i < friendNames.length; i++) {
+    console.log(friendNames[i]);
+}
---
+++ Q10,Q11/main.ts
@@ -0,0 +1,31 @@
+/*Q10:Adding Comments: Choose two of the programs you’ve written,
+ and add at least one comment to each. If you don’t have anything
+specific to write because your programs are too simple at this point,
+just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.*/
+ 
+
+ /*program:1`
+  name is Mahjabeen
+  Date:01/3/2024
+  This program will add two digits.*/
+  console.log(2 + 2);
+
+
+
+  //program:2
+  //My name is Mahjabeen
+  //Date:01/3/2024
+  //This program will write Hello World!
+  console.log("Hello World");
+
+
+
+  
+/*Q11:Names: Store the names of a few of your friends in a array called names.
+ Print each person’s name by accessing each element in the list, one at a time.*/
+
+let friendNames: string[] =['Sana','Any','Umaima','Ayna'];
+for(let i=0; i<friendNames.length; i++){
+    console.log(friendNames[i]);
+}
