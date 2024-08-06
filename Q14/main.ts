86472fb9060df2cc77dd041721070ab23709a1cb
Mahjabeen <161134560+Mahjabeen456@users.noreply.github.com>
2024-08-07 02:20:28 +0500

    Add files via upload

---
+++ Q14/main.ts
@@ -0,0 +1,10 @@
+/*Q14:Guest List: If you could invite anyone, living or deceased, to dinner, 
+who would you invite? Make a list that includes at least three people you’d 
+like to invite to dinner. Then use your list to print a message to each person, 
+inviting them to dinner.*/
+
+
+let guest_list: string[] = ['Sana','Any','Umaima','Yusra'];
+for(let i=0; i<guest_list.length; i++){
+    console.log('Dear Friends:' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\nThank You\n');
+}
