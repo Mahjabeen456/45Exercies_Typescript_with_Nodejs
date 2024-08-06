10cd77e62b58adb1fc817cce30be7ca8c268f0a0
Mahjabeen <161134560+Mahjabeen456@users.noreply.github.com>
2024-08-07 02:39:52 +0500

    Add files via upload

---
+++ Q21/main.js
@@ -0,0 +1,9 @@
+/*Q21:They think of something you could store in a TypeScript Object.
+ Write a program that creates Objects containing these items.*/
+var stuDetil = {
+    Name: "Hassan",
+    Age: 15,
+    Gender: "Male",
+    ROLLNo: 2345
+};
+console.log(stuDetil);
---
+++ Q21/main.ts
@@ -0,0 +1,20 @@
+/*Q21:They think of something you could store in a TypeScript Object.
+ Write a program that creates Objects containing these items.*/
+
+
+interface stuDetil {
+
+    Name:   string;
+    Age:    number;
+    Gender: string;
+    RollNo: number;
+}
+
+let stuDetil ={
+    Name: "Hassan",
+    Age:   15,
+    Gender:"Male",
+    ROLLNo: 2345
+};
+    
+console.log(stuDetil);
